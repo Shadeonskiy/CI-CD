03c8d48734776fe8594518e48f57e400cde61812
Shadeonskiy <shadeonskiy@gmail.com>
2023-03-28 12:30:30 +0300

    Added text cleaning from trailing whitespaces

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-def read_from_files(file1, file2):
+def read_from_files(file1, file2) -> tuple:
     with open(file1, 'r') as file1, open(file2, 'r') as file2:
-        f1_info = file1.readlines()
-        f2_info = file2.readlines()
+        f1_info = clean_text(file1.readlines())
+        f2_info = clean_text(file2.readlines())
+
     return f1_info, f2_info
+
+
+def clean_text(text: list[str]) -> list[str]:
+    return [line.rstrip('\n') for line in text]
 
 
 if __name__ == "__main__":
