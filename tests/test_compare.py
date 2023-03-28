37da0c29b7e134a604a44fa3d23cc411e4ac2251
Shadeonskiy <shadeonskiy@gmail.com>
2023-03-28 13:31:03 +0300

    Added html report and requirements files

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -3,8 +3,8 @@ from main import compare_files, read_from_files
 
 
 def test_compare_files(prepare_text_files):
-    expected = ({"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}, 
-                {"Fusce eu commodo arcu. Phasellus imperdiet convallis turpis.", 
+    expected = ({"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+                {"Fusce eu commodo arcu. Phasellus imperdiet convallis turpis.",
                  "Phasellus imperdiet convallis turpis.",
                  "Fusce eu commodo arcu."})
     f1_info, f2_info = read_from_files(*prepare_text_files)
